f974ffb99f60f4f4354014806e8ba896452431b5
Alan Fung-Schwarz <alan@atomsign.net>
2013-03-25 22:19:32 +0000

    Isolate namespace Surveyor to see if it prevents Surveyor from screwing up Bootstrap looks

--- lib/surveyor/engine.rb
+++ lib/surveyor/engine.rb
@@ -4,6 +4,7 @@ require 'haml' # required for view resolution
 
 module Surveyor
   class Engine < Rails::Engine
+    isolate_namespace Surveyor
     root = File.expand_path('../../', __FILE__)
     config.autoload_paths << root
   end
